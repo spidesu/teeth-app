614e97bd9470f75541ee8372d1e02f3f1a2bb421
unknown <Work@DESIGN-2.RST.EDU>
2019-09-04 15:04:04 +0400

    Triggers and API ready

--- app/Http/Controllers/BrushController.php
+++ app/Http/Controllers/BrushController.php
@@ -5,29 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Brush;
+use App\ToothBrush;
+use App\User;
 use Carbon\Carbon;
 class BrushController extends Controller
 {
     //
     public function index(Request $request) {
-        $id = Auth::id();
-        $brushes = App\Brush::where('brush_id', $id)->get();
-
-        dd(response()->json($brushes));
-        return response()->json($brushes);
+        $id = User::where('id', 12)->pluck('tooth_brush_id')->first();
+        $brushes = "App\Brush"::where('tooth_brush_id', $id)->get(['started_at', 'ended_at']);
+        return response()->json(['tooth_brush_id' => $id, 'brushes' => $brushes]);
     }
 
-    public function took(Request $request) {
-        dd($request);
+    public function took($tooth_brush_id) {
+
         $brush = new Brush;
         $brush->started_at = Carbon::now();
-        $brush->brush_id = $request->brush_id;
+        $brush->tooth_brush_id = $tooth_brush_id;
         $brush->save();
     }
 
     public function put(Request $request) {
-        $brush = Brush::where('brush_id',$request->brush_id)->latest()->first();
+        $brush = Brush::where('tooth_brush_id',$request->brush_id)->latest()->first();
         $brush->ended_at = Carbon::now();
-        $brush-save();
+        $brush->save();
+
     }
 }
